0085141272a832f1df92f1bd5bbc72988a219db8
codecraft26 <codescraft26@gmail.com>
2022-02-23 10:13:46 +0530

    splash screen final

--- app/src/main/java/dev/aman/myshoppe/SplashActivity.kt
+++ app/src/main/java/dev/aman/myshoppe/SplashActivity.kt
@@ -1,11 +1,39 @@
 package dev.aman.myshoppe
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.os.postDelayed
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        @Suppress("DEPRRECTED")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+
+                )
+        }
+
+        Handler().postDelayed({
+            val mIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }, 2000)
     }
+
+
+
+
 }
+
